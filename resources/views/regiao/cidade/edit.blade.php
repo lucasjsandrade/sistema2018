d5a73a677e2fbbfd523856e4c28094b4b527bd99
lucasjsandrade <lucas.andrade@edu.unipar.br>
2018-05-03 10:16:40 -0300

    bugue tela cidade

--- resources/views/regiao/cidade/edit.blade.php
+++ resources/views/regiao/cidade/edit.blade.php
@@ -16,17 +16,24 @@
   {!!Form::model($cidade, ['method'=>'PUT', 'route'=>['cidade.update', $cidade->idcidade]])!!}
   {{Form::token()}}
 
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <div class="form-group">
-     <label for="nome">Nome da Cidade</label>
-     <span class="ob">*</span>
-     <input type="text" name="nomeCidade" class="form-control" 
-     value="{{ $cidade->nomeCidade }}"
-     placeholder="Nome cidade...">
-   </div>
-  </div>
+  <div class="form-group">
+   <label for="nome">NomeCidade</label>
+   <span class="ob">*</span>
+   <input type="text" name="nomeCidade" class="form-control" 
+   value="{{ $cidade->nomeCidade }}"
+   placeholder="Nome cidade...">
+ </div>
 
-<<<<<<< HEAD
+
+ <div class="form-group">
+  <label>Estado</label>
+  <select name="idestado" class="form-control">
+    <span class="ob">*</span>
+
+    @foreach($estado as $est)
+
+    @if($est->idestado==$cidade->idestado)
+
     <option value="{{$est->idestado}}" selected><!-- Aqui vai recuperar o objeto do banco -->
       {{$est->nomeEstado}}
     </option>
@@ -37,58 +44,11 @@
     @endif
     @endforeach
   </select>
-</div>
-=======
- <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
-   <div class="form-group">
-    <label>Estado</label>
-    <select name="idestado" class="form-control">
-      <span class="ob">*</span>
-      @foreach($estado as $est)
-      @if($est->idestado==$cidade->idestado)
-      <option value="{{$est->idestado}}" selected><!-- Aqui vai recuperar o objeto do banco -->
-        {{$est->nomeEstado}}
-      </option>
-      @else
-      <option value="{{$est->idestado}}">
-        {{$est->nomeEstado}}
-      </option>
-      @endif
-      @endforeach
-    </select>
-  </div>
- </div>
-
- <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-         <div class="form-group">
-             <a href=/regiao/estado/create target="_blank"><button class="btn btn-primary" type="button" style="
-              position: absolute;
-              top:25px;
-              left: 0px;
-              "/> Novo estado </button></a>
-         </div>
-  </div>
-
-
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <div class="form-group">
-      <label for="status">Status</label>
-      <span class="ob">*</span>
-      <select name="status"  class="form-control">
-        <option value="{{$cidade->status}}">{{$cidade->status}}</option>
-        <option value="ativo">Ativo</option> 
-        <option value="Inativo">Inativo</option>
-      </select>
-    </div>
-  </div>
-
-  
-
->>>>>>> b0783e846d64c9e6259472f7d99dc1bc45f98431
 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
   <div class="form-group">
    <button class="btn btn-success" type="submit"><i class="fa fa-save"></i> Confirmar</button>
-   <button class="btn btn-danger" type="reset"  onclick="javascript: location.href='/regiao/cidade';">Cancelar</button>   
+   <button class="btn btn-danger" type="reset"  onclick="javascript: location.href='/regiao/cidade';">Cancelar</button>
+   <a href=/regiao/estado/create target="_blank"><button class="btn btn-primary" type="button">Novo Estado </button></a>
  </div>
 </div>
 
@@ -96,7 +56,7 @@
 
 </div>
 
-{!!Form::close()!!}		
+{!!Form::close()!!}   
 
 </div>
 </div>
--- storage/framework/views/9e9ec8f1170f53b16491d46af292a245c40fc88b.php
+++ storage/framework/views/9e9ec8f1170f53b16491d46af292a245c40fc88b.php
@@ -47,7 +47,6 @@
     <?php endif; ?>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
   </select>
-</div>
 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
   <div class="form-group">
    <button class="btn btn-success" type="submit"><i class="fa fa-save"></i> Confirmar</button>
@@ -60,7 +59,7 @@
 
 </div>
 
-<?php echo Form::close(); ?>		
+<?php echo Form::close(); ?>   
 
 </div>
 </div>
