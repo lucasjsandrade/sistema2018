01d431a215746e19f9403d9731eb18543fcb8f00
lucasjsandrade <lucas.andrade@edu.unipar.br>
2018-08-20 21:29:18 -0300

    Status contasp/r

--- resources/views/contasreceber/index.blade.php
+++ resources/views/contasreceber/index.blade.php
@@ -1,76 +1,90 @@
 @extends('layouts.admin')
 @section('conteudo')
-	<div class="row">
-		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-			<h3>CONTAS A RECEBER   <a href="contasreceber/create"><button class="btn btn-success">Incluir</button></a></h3>
-			@include('contasreceber.search')
-		</div>
-	</div>
+    <div class="row">
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+            <h3>CONTAS A RECEBER <a href="contasreceber/create">
+                    <button class="btn btn-success">Incluir</button>
+                </a></h3>
+            @include('contasreceber.search')
+        </div>
+    </div>
     <?php
-    function converteData($data){
-        return  $data <> "" ? date('d/m/Y', strtotime($data)) : $data = null;
+    function converteData($data)
+    {
+        return $data <> "" ? date('d/m/Y', strtotime($data)) : $data = null;
     }
     ?>
 
-	<div class="row">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			<div class="table-responsive">
-				<table class="table table-striped table-bordered table-condensed table-hover">
-					<thead>
-					<th>Id</th>
-					<th>Data</th>
-					<th>Valor</th>
-					<th>Descricao</th>
-					<th>N de parcela</th>
-					<th>Id-Venda</th>
-					<th>Id-Cliente</th>
-<<<<<<< HEAD
-				</thead>
-				@foreach ($contasreceber as $c)
-				<tr>
-					<td>{{ $c->idcontasr}}</td>
-					<td>{{converteData($c->data)}}</td>
-					<td>{{ $c->valor}}</td>
-					<td>{{ $c->descricao}}</td>
-					<td>{{ $c->numeroDeParcelas}}</td>
-					<td>{{ $c->idvenda}}</td>
-					<td>{{ $c->idcliente}}</td>
-					<td>
-						<a href="{{URL::action('ContasreceberController@show',$c->idcontasr)}}"><button class="btn btn-info">Mostrar</button></a>
-						<a href="{{URL::action('desenvolvimentoController@index')}}"><button class="btn btn-info">Alterar</button></a>
-						<a href="" data-target="#modal-delete-{{$c->idcontasr}}" data-toggle="modal"><button class="btn btn-danger">Excluir</button></a>
-					</td>
-				</tr>
-				@include('contasreceber.modal')
-				@endforeach
-			</table>
-		</div>
-=======
-					</thead>
-					@foreach ($contasreceber as $c)
-						<tr>
-							<td>{{ $c->idcontasr}}</td>
-							<td>{{converteData( $c->data)}}</td>
-							<td>{{ $c->valor}}</td>
-							<td>{{ $c->descricao}}</td>
-							<td>{{ $c->numeroDeParcelas}}</td>
-							<td>{{ $c->idvenda}}</td>
-							<td>{{ $c->idcliente}}</td>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-condensed table-hover">
+                    <thead>
+                    <th>Id</th>
+                    <th>Data</th>
+                    <th>Valor</th>
+                    <th>Descricao</th>
+                    <th>N de parcela</th>
+                    <th>Id-Venda</th>
+                    <th>Id-Cliente</th>
 
->>>>>>> d82e173793cf1605e7aa9cda45dea3877999f1c5
+                    </thead>
+                    @foreach ($contasreceber as $c)
+                        <tr>
+                            <td>{{ $c->idcontasr}}</td>
+                            <td>{{converteData($c->data)}}</td>
+                            <td>{{ $c->valor}}</td>
+                            <td>{{ $c->descricao}}</td>
+                            <td>{{ $c->numeroDeParcelas}}</td>
+                            <td>{{ $c->idvenda}}</td>
+                            <td>{{ $c->idcliente}}</td>
+                            <td>
+                                <a href="{{URL::action('ContasreceberController@show',$c->idcontasr)}}">
+                                    <button class="btn btn-info">Mostrar</button>
+                                </a>
+                                <a href="{{URL::action('desenvolvimentoController@index')}}">
+                                    <button class="btn btn-info">Alterar</button>
+                                </a>
+                                <a href="" data-target="#modal-delete-{{$c->idcontasr}}" data-toggle="modal">
+                                    <button class="btn btn-danger">Excluir</button>
+                                </a>
+                            </td>
+                        </tr>
+                        @include('contasreceber.modal')
+                    @endforeach
+                </table>
+            </div>
 
-							<td>
-								<a href="{{URL::action('ContasreceberController@show',$c->idcontasr)}}"><button class="btn btn-info">Mostrar</button></a>
-								<a href="{{URL::action('desenvolvimentoController@index')}}"><button class="btn btn-info">Alterar</button></a>
-								<a href="" data-target="#modal-delete-{{$c->idcontasr}}" data-toggle="modal"><button class="btn btn-danger">Excluir</button></a>
-							</td>
-						</tr>
-						@include('contasreceber.modal')
-					@endforeach
-				</table>
-			</div>
+            </thead>
+            @foreach ($contasreceber as $c)
+                <tr>
+                    <td>{{ $c->idcontasr}}</td>
+                    <td>{{converteData( $c->data)}}</td>
+                    <td>{{ $c->valor}}</td>
+                    <td>{{ $c->descricao}}</td>
+                    <td>{{ $c->numeroDeParcelas}}</td>
+                    <td>{{ $c->idvenda}}</td>
+                    <td>{{ $c->idcliente}}</td>
 
-			{{$contasreceber->render()}}
-		</div>
-	</div>
+
+                    <td>
+                        <a href="{{URL::action('ContasreceberController@show',$c->idcontasr)}}">
+                            <button class="btn btn-info">Mostrar</button>
+                        </a>
+                        <a href="{{URL::action('desenvolvimentoController@index')}}">
+                            <button class="btn btn-info">Alterar</button>
+                        </a>
+                        <a href="" data-target="#modal-delete-{{$c->idcontasr}}" data-toggle="modal">
+                            <button class="btn btn-danger">Excluir</button>
+                        </a>
+                    </td>
+                </tr>
+                @include('contasreceber.modal')
+                @endforeach
+                </table>
+        </div>
+
+        {{$contasreceber->render()}}
+    </div>
+    </div>
 @stop
